c2daa115a35143d6c7c718ed639fc735d7a74a61
moax101 <32432153+moax101@users.noreply.github.com>
2020-12-28 05:13:04 -0800

    apr crud (#1)

---
+++ resources/views/apr/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app', ['page' => __('Method Management'), 'pageSlug' => 'apr', 'section' => 'transactions'])
+
+@section('content')
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-12 order-xl-1">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">{{ __('Method Management') }}</h3>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="{{ route('apr.index') }}" class="btn btn-sm btn-primary">{{ __('Back to list') }}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="{{ route('apr.update', $aprs) }}" autocomplete="off">
+                            @csrf
+                            @method('put')
+
+                            <h6 class="heading-small text-muted mb-4">{{ __('Product information') }}</h6>
+                            <div class="pl-lg-4">
+                                <div class="form-group{{ $errors->has('item_no') ? ' has-danger' : '' }}">
+                                    <label class="form-control-label" for="input-item_no">{{ __('Name') }}</label>
+                                    <input type="text" name="item_no" id="input-item_no" class="form-control form-control-alternative{{ $errors->has('item_no') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('item_no', $aprs->item_no) }}" required autofocus>
+                                    @include('alerts.feedback', ['field' => 'item_no'])
+                                </div>
+                                <div class="form-group{{ $errors->has('item_desc') ? ' has-danger' : '' }}">
+                                    <label class="form-control-label" for="input-item_desc">{{ __('Description') }}</label>
+                                    <input type="text" name="item_desc" id="input-item_desc" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('item_desc', $aprs->item_desc) }}" required>
+                                    @include('alerts.feedback', ['field' => 'item_desc'])
+                                </div>
+
+
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
